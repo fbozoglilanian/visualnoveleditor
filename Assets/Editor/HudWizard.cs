be645a29d89aa4e4f3c7c36c832b7f561d801c9f
Fabian Bozoglilalian <fabian.bozoglian@gmail.com>
2017-09-20 10:56:25 -0300

    Alpha Editor

---
+++ Assets/Editor/HudWizard.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class HudWizard : ScriptableWizard 
+{
+	public enum SizeValue {SMALL, MEDIUM, BIG};
+	public enum Position {LEFT, MIDDLE, RIGHT};
+
+
+	private DialogHud dialogHud;
+
+	public Sprite backgroundTexture;
+	public Color backgroundColor = new Color32( 0x90, 0x90, 0x90, 0xFF );
+
+	public Sprite characterTexture;
+	public Position characterPosition = Position.MIDDLE;
+
+	public bool showCharacterName = true;
+	public Color characterNameColor = new Color32( 0xFF, 0xFF, 0xFF, 0x64 ); 
+	public Font characterNameFont;
+	//[Range(1,120)]
+	private int characterNameFontSize = 14;
+
+	public SizeValue characterNameSize = SizeValue.SMALL;
+
+
+	public Color dialogColor = new Color32( 0xFF, 0xFF, 0xFF, 0x64 );
+	public Font dialogFont;
+	[Range(1,120)]
+	public int dialogFontSize = 14;
+
+	public Color optionsColor = new Color32( 0xFF, 0xFF, 0xFF, 0x64 );
+	public Font optionsFont;
+	[Range(1,120)]
+	public int optionsFontSize = 14;
+
+
+
+    [MenuItem ("My Tools/Hud Wizard")]
+    static void CreateWizard()
+    {
+        HudWizard hudWizard = ScriptableWizard.DisplayWizard<HudWizard> ("Create Hud", "Create", "Update");
+		if (GameObject.Find ("Dialog-Hud")) {
+			hudWizard.dialogHud = (DialogHud)((GameObject)GameObject.Find ("Dialog-Hud")).GetComponent<DialogHud> ();
+			hudWizard.backgroundTexture = hudWizard.dialogHud.background.sprite;
+			hudWizard.characterTexture = hudWizard.dialogHud.character.sprite;
+			hudWizard.showCharacterName = hudWizard.dialogHud.characterNamePanel.active;
+			hudWizard.backgroundColor = hudWizard.dialogHud.background.color;
+
+			//Dialog components colors
+			hudWizard.dialogColor = hudWizard.dialogHud.dialogPanel.GetComponent<Image>().color;
+			hudWizard.characterNameColor = hudWizard.dialogHud.characterNamePanel.GetComponent<Image>().color;
+			hudWizard.optionsColor = hudWizard.dialogHud.optionPanel.GetComponent<Image>().color;
+
+			//Current font size
+			hudWizard.characterNameFontSize = hudWizard.dialogHud.characterNamePanel.gameObject.GetComponentInChildren<Text> ().fontSize;
+			hudWizard.dialogFontSize = hudWizard.dialogHud.dialogPanel.gameObject.GetComponentInChildren<Text> ().fontSize;
+			hudWizard.optionsFontSize = hudWizard.dialogHud.optionPanel.gameObject.GetComponentInChildren<Text> ().fontSize;
+
+			//Current font type
+			hudWizard.characterNameFont = hudWizard.dialogHud.characterNamePanel.gameObject.GetComponentInChildren<Text> ().font;
+			hudWizard.dialogFont = hudWizard.dialogHud.dialogPanel.gameObject.GetComponentInChildren<Text> ().font;
+			hudWizard.optionsFont = hudWizard.dialogHud.optionPanel.gameObject.GetComponentInChildren<Text> ().font;
+		}
+    }
+
+
+    void OnWizardCreate()
+    {
+		if (dialogHud == null) {
+			DestroyImmediate (dialogHud);
+		}
+		dialogHud = (DialogHud) ((GameObject) Instantiate (Resources.Load ("DialogHud"))).GetComponent<DialogHud>();
+		dialogHud.name = "Dialog-Hud";
+
+
+		if (backgroundTexture != null) {
+			dialogHud.background.sprite = backgroundTexture;
+		}
+		if (characterTexture != null) {
+			dialogHud.character.sprite = characterTexture;
+			UpdateCharacterPosition ();
+		}
+		dialogHud.characterNamePanel.SetActive (this.showCharacterName);
+		UpdateSizes ();
+		UpdateFonts ();
+		UpdateColors ();
+    }
+
+	void OnWizardOtherButton()
+	{
+		if (GameObject.Find ("Dialog-Hud")) {
+			dialogHud = (DialogHud)((GameObject)GameObject.Find ("Dialog-Hud")).GetComponent<DialogHud> ();
+		} else {
+			dialogHud = (DialogHud) ((GameObject) Instantiate (Resources.Load ("DialogHud"))).GetComponent<DialogHud>();
+			dialogHud.name = "Dialog-Hud";
+		}
+
+		dialogHud.background.sprite = backgroundTexture;
+		dialogHud.character.sprite = characterTexture;
+		UpdateCharacterPosition ();
+		dialogHud.characterNamePanel.SetActive (this.showCharacterName);
+		UpdateSizes ();
+		UpdateFonts ();
+		UpdateColors ();
+
+		EditorUtility.SetDirty(dialogHud);
+	}
+
+
+	void UpdateCharacterPosition()
+	{
+		switch (characterPosition) {
+		case Position.LEFT:
+			dialogHud.character.gameObject.GetComponent<RectTransform> ().anchoredPosition = new Vector2(0, 0);
+			break;
+		case Position.MIDDLE:
+			dialogHud.character.gameObject.GetComponent<RectTransform> ().anchoredPosition = new Vector2(280, 0);
+			break;
+		case Position.RIGHT:
+			dialogHud.character.gameObject.GetComponent<RectTransform> ().anchoredPosition = new Vector2(550, 0);
+			break;
+		}
+	}
+
+	void UpdateFonts()
+	{
+		//Font Size
+		dialogHud.characterNamePanel.gameObject.GetComponentInChildren<Text> ().fontSize = this.characterNameFontSize;
+		dialogHud.dialogPanel.gameObject.GetComponentInChildren<Text> ().fontSize = this.dialogFontSize;
+		dialogHud.optionPanel.gameObject.GetComponentInChildren<Text> ().fontSize = this.optionsFontSize;
+
+		//Font Type
+		if (characterNameFont != null) {
+			dialogHud.characterNamePanel.gameObject.GetComponentInChildren<Text> ().font = this.characterNameFont;
+		}
+		if (dialogFont != null) {
+			dialogHud.dialogPanel.gameObject.GetComponentInChildren<Text> ().font = this.dialogFont;
+		}
+		if (optionsFont != null) {
+			dialogHud.optionPanel.gameObject.GetComponentInChildren<Text> ().font = this.optionsFont;
+		}
+	}
+
+	void UpdateColors()
+	{
+		dialogHud.background.color = backgroundColor;
+		dialogHud.dialogPanel.GetComponent<Image>().color = dialogColor;
+		dialogHud.characterNamePanel.GetComponent<Image>().color = characterNameColor;
+		dialogHud.optionPanel.GetComponent<Image>().color = optionsColor;
+	}
+
+	void UpdateSizes()
+	{
+		int height = 30;
+		int width = 100;
+		switch (characterNameSize) {
+		case SizeValue.SMALL:
+			characterNameFontSize = 14;
+			break;
+		case SizeValue.MEDIUM:
+			characterNameFontSize = 20;
+			height = 40;
+			width = 160;
+			break;
+		case SizeValue.BIG:
+			characterNameFontSize = 40;
+			height = 60;
+			width = 200;
+			break;
+		}
+
+		dialogHud.characterNamePanel.gameObject.GetComponent<RectTransform> ().sizeDelta = new Vector2(width, height);
+	}
+
+
+    void OnWizardUpdate()
+    {
+		
+    }
+
+}
